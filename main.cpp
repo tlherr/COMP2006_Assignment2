3d495930bb59f9e08a7bb713848a7c68a1b6edec
Thomas Herr <thom1810@gmail.com>
2017-05-29 15:19:22 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Student Name: Thomas Herr
+ * Student Number: 200325519
+ * 17S Introduction to C++ - 01 (COMP2006-17S-30694)
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
